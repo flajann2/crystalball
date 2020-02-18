d4faf8ea758800ff695ee83b5707aa8b19fbfaa8
Fred Mitchell <fred.mitchell@gmx.de>
2020-02-18 11:06:54 +0100

    the very beginnings of the spring boot integration

---
+++ src/main/java/de/zuseneat/crystalball/CrystalballApplication.java
@@ -0,0 +1,13 @@
+package de.zuseneat.crystalball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrystalballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/zuseneat/crystalball/CrystalballApplicationTests.java
@@ -0,0 +1,13 @@
+package de.zuseneat.crystalball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
